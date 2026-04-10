b4e28f1658695cd17e9af970bd8a677a91871ffc
Keith Robertson <info@12stonecosulting.com>
2026-04-10 18:50:18 -0400

    Revert state field back to text input

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -199,31 +199,14 @@ const payload = {
 
               <div>
   <label htmlFor="state">State</label>
-  <select
-    id="state"
-    name="state"
-    value={formData.state}
-    onChange={handleChange}
-  >
-    <option value="">Select your state</option>
-    <option>Alabama</option><option>Alaska</option><option>Arizona</option>
-    <option>Arkansas</option><option>California</option><option>Colorado</option>
-    <option>Connecticut</option><option>Delaware</option><option>Florida</option>
-    <option>Georgia</option><option>Hawaii</option><option>Idaho</option>
-    <option>Illinois</option><option>Indiana</option><option>Iowa</option>
-    <option>Kansas</option><option>Kentucky</option><option>Louisiana</option>
-    <option>Maine</option><option>Maryland</option><option>Massachusetts</option>
-    <option>Michigan</option><option>Minnesota</option><option>Mississippi</option>
-    <option>Missouri</option><option>Montana</option><option>Nebraska</option>
-    <option>Nevada</option><option>New Hampshire</option><option>New Jersey</option>
-    <option>New Mexico</option><option>New York</option><option>North Carolina</option>
-    <option>North Dakota</option><option>Ohio</option><option>Oklahoma</option>
-    <option>Oregon</option><option>Pennsylvania</option><option>Rhode Island</option>
-    <option>South Carolina</option><option>South Dakota</option><option>Tennessee</option>
-    <option>Texas</option><option>Utah</option><option>Vermont</option>
-    <option>Virginia</option><option>Washington</option><option>West Virginia</option>
-    <option>Wisconsin</option><option>Wyoming</option>
-  </select>
+<input
+  id="state"
+  name="state"
+  type="text"
+  placeholder="Your state"
+  value={formData.state}
+  onChange={handleChange}
+/>
 </div>
            </div>
 
