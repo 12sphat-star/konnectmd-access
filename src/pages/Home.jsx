b78db034b56b4eac02e8d31c181db84cd391ed3f
Keith Robertson <info@12stonecosulting.com>
2026-04-12 16:20:54 -0400

    Add CostComparison and AgentSection to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import AgentSection from "../components/home/AgentSection";
 import Hero from "../components/home/Hero";
 import TrustStrip from "../components/home/TrustStrip";
 import WhatItIs from "../components/home/WhatItIs";
 import WhyChooseUs from "../components/home/WhyChooseUs";
-import FastAccess from "../components/home/FastAccess";
+import CostComparison from "../components/home/CostComparison";
 import WhoItsFor from "../components/home/WhoItsFor";
+import FastAccess from "../components/home/FastAccess";
 import HowItWorks from "../components/home/HowItWorks";
 import PlansPreview from "../components/home/PlansPreview";
 import AddOnsSection from "../components/home/AddOnsSection";
 import Disclaimer from "../components/shared/Disclaimer";
+import AgentSection from "../components/home/AgentSection";
 import LaunchSection from "../components/home/LaunchSection";
 import CTASection from "../components/home/CTASection";
+
 
 export default function Home() {
   return (
@@ -19,6 +21,7 @@ export default function Home() {
       <TrustStrip />
       <WhatItIs />
   <WhyChooseUs />
+  <CostComparison />
 <WhoItsFor />
 <FastAccess />
 <HowItWorks />
