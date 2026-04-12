1ee1266ff7d1731246e8e3786dcb835ddf2a8a2c
Keith Robertson <info@12stonecosulting.com>
2026-04-12 14:39:40 -0400

    Add ScrollToTop to fix page navigation scroll position

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import JoinPage from "./pages/JoinPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
@@ -7,18 +8,28 @@ import PlansPage from "./pages/PlansPage";
 import ContactPage from "./pages/ContactPage";
 import CalendarPage from "./pages/CalendarPage";
 import LandingPage from "./pages/LandingPage";
+import JoinPage from "./pages/JoinPage";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Navbar />
       <Routes>
-        <Route path="/join-the-team" element={<JoinPage />} />
         <Route path="/" element={<Home />} />
         <Route path="/plans" element={<PlansPage />} />
         <Route path="/get-details" element={<ContactPage />} />
         <Route path="/book-call" element={<CalendarPage />} />
         <Route path="/landing" element={<LandingPage />} />
+        <Route path="/join-the-team" element={<JoinPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
